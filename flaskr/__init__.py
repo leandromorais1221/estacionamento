0774b53f3e1f425e4e0ee910963aba3d5c1f9994
Leandro Morais <leandromorais1221@gmail.com>
2026-05-01 13:05:19 -0300

    chore: atualiza para conectar no banco e criar tabelas

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flaskr.controllers.site_controller import site_bp
+from flaskr.models import *
 
 def create_app():
     app = Flask(__name__, template_folder="./views")
     app.secret_key = "b215e562c033ef21a689f7963ad99c97"
     app.register_blueprint(site_bp)
+
+    db.connect()
+    db.create_tables([Proprietario, Tipo, Cor, Veiculo, Movimentacao, TabelaPreco], safe=True)
 
     return app
